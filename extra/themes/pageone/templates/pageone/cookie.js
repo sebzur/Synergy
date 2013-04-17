18365e6e40747b2984dbd0ec545060e038bc3e5f
CodeLAB <codelab@codelab.pl>
2013-04-17 19:58:40 +0200

    Cookie politics generalized

---
+++ extra/themes/pageone/templates/pageone/cookie.js
@@ -0,0 +1,9 @@
+    <script>
+      $(document).ready(function () {
+      // activate cookie cutter
+      $.cookieCuttr({
+      cookieDeclineButton: false,
+      cookieAnalytics: false,
+      });
+      });    
+    </script>
